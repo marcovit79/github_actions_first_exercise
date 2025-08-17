7bfc9a727c70f61761640f64827ca3337fff0004
Marco Vit <marcovit79@gmail.com>
2025-08-17 22:11:40 +0200

    lambda with one layer

--- lambdas/node/list-vpcs/src/index.ts
+++ lambdas/node/list-vpcs/src/index.ts
@@ -1,4 +1,19 @@
+import { EC2Client, DescribeVpcsCommand } from "@aws-sdk/client-ec2";
+
 exports.handler = async function () {
   
-  console.log("Hello world");
+  const client = new EC2Client({});
+    const { Vpcs } = await client.send(
+      new DescribeVpcsCommand({
+        Filters: [{ Name: "is-default", Values: ["true"] }],
+      }),
+    );
+    
+    if ( Vpcs ) {
+      console.log( "Default VPC cidr = ", Vpcs[0].CidrBlock )
+    }
+    else {
+      console.log( "No default VPC" )
+    }
+
 };
